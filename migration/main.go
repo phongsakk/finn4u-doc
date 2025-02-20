9f81a447d8815daf30081101214305726f628486
Mee(TB) <phongsak.ysd@gmail.com>
2025-02-20 23:40:57 +0700

    seed district

--- migration/main.go
+++ migration/main.go
@@ -13,24 +13,73 @@ import (
 )
 
 func main() {
-	fmt.Println("Migrating")
-	jsonFile, err := os.Open("./json/province.json")
+	// if err := SeedProvince(); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// if err := SeedDistrict(); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	return
+}
+
+func SeedDistrict() error {
+	jsonFile, err := os.Open("./json/district.json")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	defer jsonFile.Close()
 
 	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println(err)
+		return err
+	}
+	var file FileDistrict
+	json.Unmarshal(byteValue, &file)
+
+	db, err := Conn()
+	if err != nil {
+		return err
+	}
+	defer Close(db)
+	if err := db.Transaction(func(tx *gorm.DB) error {
+		for _, district := range file.Data {
+			var insert models.District = models.District{}
+			insert.ID = uint(district.Id)
+			insert.Name = district.NameTh
+			insert.NameEn = district.NameEn
+			insert.ProvinceID = uint(district.ProvinceId)
+			if err := db.Create(&insert).Error; err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SeedProvince() error {
+	fmt.Println("Migrating")
+	jsonFile, err := os.Open("./json/province.json")
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return err
 	}
 	var file FileProvince
 	json.Unmarshal(byteValue, &file)
 
 	db, err := Conn()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	defer Close(db)
 	for _, province := range file.Data {
@@ -38,8 +87,11 @@ func main() {
 		insert.Name = province.NameTh
 		insert.NameTh = province.NameTh
 		insert.NameEn = province.NameEn
-		db.Create(&insert)
+		if err := db.Create(&insert).Error; err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func Conn() (*gorm.DB, error) {
@@ -73,4 +125,16 @@ type JsonProvinces struct {
 	CreatedAt   string  `json:"created_at"`
 	UpdatedAt   string  `json:"updated_at"`
 	DeletedAt   *string `json:"deleted_at"`
+}
+type FileDistrict struct {
+	Data []JsonDistricts `json:"data"`
+}
+type JsonDistricts struct {
+	Id         int     `json:"id"`
+	NameTh     string  `json:"name_th"`
+	NameEn     string  `json:"name_en"`
+	ProvinceId int     `json:"province_id"`
+	CreatedAt  string  `json:"created_at"`
+	UpdatedAt  string  `json:"updated_at"`
+	DeletedAt  *string `json:"deleted_at"`
 }
--- migration/models/district.go
+++ migration/models/district.go
@@ -9,4 +9,6 @@ func (District) TableName() string {
 type District struct {
 	template.Model
 	template.NameMultiLanguage
+	ProvinceID uint     `json:"province_id" gorm:"not null"`
+	Province   Province `json:"province" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ProvinceID"`
 }
