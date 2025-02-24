1e071916b31e517aa22e89a61f6e2278ccd33f80
Mee(TB) <phongsak.ysd@gmail.com>
2025-02-24 15:24:14 +0700

    active

--- admin/src/component/layout/Sidebar.tsx
+++ admin/src/component/layout/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import Finn4ULogo from "@/assets/img/finn4u-logo.png"
 import Icon1 from "@/assets/img/icon1.png";
 import Icon2 from "@/assets/img/icon2.png";
@@ -8,49 +9,36 @@ import Icon5 from "@/assets/img/icon5.png";
 import Icon6 from "@/assets/img/icon6.png";
 import Icon7 from "@/assets/img/icon7.png";
 import Icon8 from "@/assets/img/icon8.png";
-import Image, { StaticImageData } from 'next/image'
-import Link from 'next/link';
-import type { UrlObject } from 'url';
+import SidebarItem from "@/component/layout/SidebarItem"
 
-type Url = string | UrlObject;
-
-type SidebarItemProps = {
-	icon: StaticImageData;
-	label: string;
-	isActive?: boolean;
-	href: Url;
-}
-const SidebarItem: React.FC<SidebarItemProps> = ({ icon, label, href, isActive = false }) => {
-	return (
-		<>
-			<li className={isActive ? "sidebar-item active" : "sidebar-item"}>
-				<Link className="sidebar-link" href={href}>
-					<Image src={icon} className="a" alt={label} />
-					<span className="align-middle">{label}</span>
-				</Link>
-			</li>
-		</>
-	)
-}
+const sidebarItems = [
+	{ icon: Icon1, label: "Overview", href: "/", isActive: false },
+	{ icon: Icon2, label: "Matching", href: "/matching", isActive: false },
+	{ icon: Icon3, label: "ผู้ขายฝาก", href: "/consigment", isActive: false },
+	{ icon: Icon4, label: "นักลงทุน", href: "/invester", isActive: false },
+	{ icon: Icon5, label: "ทรัพย์สินขายฝาก", href: "/property", isActive: false },
+	{ icon: Icon6, label: "Dashboard", href: "/dashboard", isActive: false },
+	{ icon: Icon7, label: "การตั้งค่า", href: "/setting", isActive: false },
+	{ icon: Icon8, label: "ออกจากระบบ", href: "/logout", isActive: false }
+]
 
 const Sidebar = () => {
 	return (
 		<nav id="sidebar" className="sidebar js-sidebar">
 			<div className="sidebar-content js-simplebar">
-
 				<a className="sidebar-brand" href="index.php">
 					<Image src={Finn4ULogo} className="a w-100 img-fluid" alt="" />
 				</a>
 
 				<ul className="sidebar-nav">
-					<SidebarItem icon={Icon1} label="Overview" href="/" />
-					<SidebarItem icon={Icon2} label="Matching" href="/matching" />
-					<SidebarItem icon={Icon3} label="ผู้ขายฝาก" href="consigment" />
-					<SidebarItem icon={Icon4} label="นักลงทุน" href="invester" />
-					<SidebarItem icon={Icon5} label="ทรัพย์สินขายฝาก" href="property" />
-					<SidebarItem icon={Icon6} label="Dashboard" href="dashboard" />
-					<SidebarItem icon={Icon7} label="การตั้งค่า" href="/" />
-					<SidebarItem icon={Icon8} label="ออกจากระบบ" href="/" />
+					{sidebarItems.map(({ href, icon, label, isActive }) => (
+						<SidebarItem
+							key={href}
+							icon={icon}
+							label={label}
+							href={href}
+						/>
+					))}
 				</ul>
 			</div>
 		</nav>
--- admin/src/component/layout/SidebarItem.tsx
+++ admin/src/component/layout/SidebarItem.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client"
+import style from "@/assets/css/sidebar.module.css";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { UrlObject } from "node:url";
+import { useMemo } from "react";
 
-const SidebarItem = () => {
+type Url = string | UrlObject;
+
+type SidebarItemProps = {
+  icon: StaticImageData;
+  label: string;
+  href: Url;
+}
+
+// Inside SidebarItem component
+const SidebarItem: React.FC<SidebarItemProps> = ({ icon, label, href }) => {
+  const pathname = usePathname();
+  const isActive = useMemo(() => {
+    const targetPath = typeof href === 'string' ? href : href.pathname;
+    const isSameRoute = pathname === targetPath
+    const toIndex = (pathname === '/' && targetPath === '')
+    return isSameRoute || toIndex;
+  }, [href, pathname]);
+
+  const activeClassName = useMemo(() =>
+    isActive ? `${style.sidebarItem} ${style.active}` : style.sidebarItem,
+    [isActive]
+  );
   return (
-    <div>SidebarItem</div>
+    <>
+      <li className={activeClassName}>
+        <Link className={style.sidebarLink} href={href}>
+          <Image src={icon} alt={label} className={style.sidebarItemImage} />
+          <span className="align-middle">{label}</span>
+        </Link>
+      </li>
+    </>
   )
 }
 
-export default SidebarItem
+export default SidebarItem;
