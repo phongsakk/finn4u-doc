d983da05ab2f65d244c7df00af2b24be204aad84
Mee(TB) <phongsak.ysd@gmail.com>
2025-03-14 00:43:28 +0700

    bash

--- deployWeb.sh
+++ deployWeb.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 git fetch && git pull && docker compose up --build -d frontend
 
-location=$(pwd)
+# ใช้ absolute path เพื่อป้องกันปัญหาเกี่ยวกับ relative path
+location=$(realpath "$(dirname "$0")")
 
-. "$location/discord.sh" "# Web is deployed\n[link](http://203.159.93.236:8079)"
+# รัน script แจ้งเตือนใน Discord
+bash "$location/discord.sh" "# Web is deployed\n[link](http://203.159.93.236:8079)"
--- discord.sh
+++ discord.sh
@@ -2,6 +2,4 @@ message="$1"
 
 curl --location 'https://discord.com/api/webhooks/1349790099218501705/1dTvD5R9e-BmHmBsDP7MTWaMFi0YJm1lAzn25Eka-4C1_nW2Rf7LZjcz5lkE055en001' \
 --header 'Content-Type: application/json' \
---data "{
-    \"content\": \"$message\"
-}"
+--data "{\"content\": \"$message\"}"
