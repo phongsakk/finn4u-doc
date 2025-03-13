70068ab10824eb4146a42705b5a3ead9997f0148
Mee(TB) <phongsak.ysd@gmail.com>
2025-03-14 00:35:10 +0700

    message

--- deployWeb.sh
+++ deployWeb.sh
@@ -1,1 +1,5 @@
 git fetch && git pull && docker compose up --build -d frontend
+
+location=$(pwd)
+
+. "$location/discord.sh" "# Web is deployed\n[link](http://203.159.93.236:8079)"
---
+++ discord.sh
@@ -0,0 +1,7 @@
+message="$1"
+
+curl --location 'https://discord.com/api/webhooks/1349790099218501705/1dTvD5R9e-BmHmBsDP7MTWaMFi0YJm1lAzn25Eka-4C1_nW2Rf7LZjcz5lkE055en001' \
+--header 'Content-Type: application/json' \
+--data "{
+    \"content\": \"$message\"
+}"
