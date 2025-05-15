7d42fa072db5ec1aeddf85af1f2d82cc4ef140b1
llOzakall <kengker1144@gmail.com>
2025-05-15 21:04:27 +0700

    fix

--- admin/src/app/(admin)/invester/layout.tsx
+++ admin/src/app/(admin)/invester/layout.tsx
@@ -7,14 +7,6 @@ export default function RootLayout({
 }>) {
   return (
     <>
-<<<<<<< HEAD
-      {children}
-      {/* <Navbar title="Matching" />
-      <main className="content">
-        <div className="container-fluid p-0 overflow-x-auto">
-        </div>
-      </main> */}
-=======
       <Navbar title="นักลงทุน" />
       <main className="content">
         <div className="container-fluid p-0 overflow-x-auto ">
@@ -23,7 +15,6 @@ export default function RootLayout({
           </div>
         </div>
       </main>
->>>>>>> 8e623a6e4ab919a4e72cf27a128f6a1c61f39f3e
     </>
   );
 }
