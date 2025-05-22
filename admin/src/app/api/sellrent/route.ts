7dcd010a8907bf9735b1c7826c3a32f3a4786ff9
llOzakall <kengker1144@gmail.com>
2025-05-22 15:56:15 +0700

    ขายเช่า admin

---
+++ admin/src/app/api/sellrent/route.ts
@@ -0,0 +1,43 @@
+import { catchError, CheckAuth, ToDateThai } from "@component/dev/Helpers";
+import { api } from "@utils/api";
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const page = Number(req.nextUrl.searchParams.get("page")) || 1;
+    const session = await CheckAuth();
+    if (!session.status) {
+      return NextResponse.json(session);
+    }
+    const { data: res } = await axios.get(
+      api.external(
+        `/v1/admin/sell?limit=20&page=${page}&order_by=created_at&sort=desc`
+      ),
+      session?.headerToken
+    );
+    return NextResponse.json(
+      {
+        status: res.status,
+        code: res.code,
+        data: res?.data?.map((item: any) => ({
+          id: item.id,
+          created_at: ToDateThai(item?.created_at, "DD/MM/BBBB HH:mm"),
+          fullname: item?.owner?.Firstname + " " + item?.owner?.Lastname,
+          title: item?.title,
+          wanted_agency: item?.wanted_agency,
+          sell_type_id: item?.sell_type_id === 1 ? "ประกาศขาย" : "ประกาศเช่า",
+          asset_type: item?.asset_type?.name,
+          is_disabled: item?.is_disabled,
+        })),
+        page: res.page,
+        total: res.total_page,
+      },
+      {
+        status: res.code,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(await catchError(error));
+  }
+};
